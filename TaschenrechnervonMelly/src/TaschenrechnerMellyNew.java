0c328a5802cf3982d092a59479c198b3837dd033
Melly19888 <139958288+Melly19888@users.noreply.github.com>
2023-07-23 21:43:32 +0200

    Add files via upload

---
+++ TaschenrechnervonMelly/src/TaschenrechnerMellyNew.java
@@ -0,0 +1,53 @@
+Import java.util.Scanner;
+
+public class TaschenrechnerMellyNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Scanner benutzereingabe = new Scanner (System.in);
+
+	        // Kommaseparierte Zahlen
+	        double ersteZahl ;
+	        double zweiteZahl;
+
+
+
+	        // Ein einziges Symbol '*', '/', '+', '-'
+	        char operator;
+
+	        System.out.println("Geben Sie Ihre erste Zahl ein:");
+	        ersteZahl = benutzereingabe.nextDouble();
+
+	        System.out.println("Geben Sie Ihre zweite Zahl ein:");
+	        zweiteZahl = benutzereingabe.nextDouble();
+
+	        System.out.println("Geben Sie bitte einen Rechenoperator ein (*, /, +, -,√ )");
+	        operator = benutzereingabe.next().charAt(0);
+
+	        if (operator == '*')
+	        {
+	            System.out.println(ersteZahl * zweiteZahl);}
+
+	        else if (operator == '/')
+	        {
+	            System.out.println(ersteZahl / zweiteZahl);}
+
+	        else if (operator == '+')
+	        {
+	            System.out.println(ersteZahl+ zweiteZahl);}
+
+	        if (operator == '-')
+	        {
+	            System.out.println(ersteZahl - zweiteZahl);}
+
+	        if (operator== '√')
+
+	            System.out.println(Math.sqrt(erstezahl));
+
+	        else
+	        {
+	            System.out.println("Ungültiger Rechenoperator");}
+
+
+
+}
